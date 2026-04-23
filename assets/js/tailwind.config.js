2f347ed0da418f6b95fba39a4345c80fea54b9a3
Youssef Ouballa <24794411+YoussefOuballa@users.noreply.github.com>
2026-04-23 22:49:04 +0100

    first commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#005EB8",
+        "primary-hover": "#004280",
+        danger: "#e62e4d",
+        dark: "#111827",
+        light: "#f3f4f6",
+      },
+      fontFamily: {
+        sans: ["Inter", "Cairo", "sans-serif"],
+        serif: ["Barlow Condensed", "Cairo", "serif"],
+      },
+    },
+  },
+};
